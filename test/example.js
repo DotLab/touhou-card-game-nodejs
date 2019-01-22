423ad923e71c101b8568c5515705e4280f716036
Kailang <kailangfu@gmail.com>
2019-01-22 12:53:53 -0600

    Add coverage generation and coveralls reporting

--- test/example.js
+++ test/example.js
@@ -1,9 +1,15 @@
+const example = require('../src/example');
 const assert = require('assert');
 
-describe('Array', function() {
+describe('Example', function() {
   describe('#indexOf()', function() {
     it('should return -1 when the value is not present', function() {
       assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+  });
+  describe('#example', function() {
+    it('should return a + b', function() {
+      assert.equal(example(1, 1), 2);
     });
   });
 });
