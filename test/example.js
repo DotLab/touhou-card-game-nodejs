1f6ce9884d8b7ec547b6b2018cc5097261505ded
Kailang <kailangfu@gmail.com>
2019-01-23 18:08:25 -0600

    Ignore line break style

--- test/example.js
+++ test/example.js
@@ -1,14 +1,14 @@
 const example = require('../src/example');
 const assert = require('assert');
 
-describe('Example', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
+describe('Example', () => {
+  describe('#indexOf()', () => {
+    it('should return -1 when the value is not present', () => {
       assert.equal([1, 2, 3].indexOf(4), -1);
     });
   });
-  describe('#example', function() {
-    it('should return a + b', function() {
+  describe('#example', () => {
+    it('should return a + b', () => {
       assert.equal(example(1, 1), 2);
     });
   });
