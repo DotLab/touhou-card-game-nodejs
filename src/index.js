3ad3a5cc983c0d1ce3b44a9083108bb9ed77bffe
Kailang <kailangfu@gmail.com>
2019-02-03 12:56:55 -0600

    Add socket.io

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 const debug = require('debug')('tcg');
-const express = require('express');
-const app = require('express')();
 
-const port = 3000;
-app.listen(port, () => debug('listening on port %d', port));
+const express = require('express');
+const app = express();
+const Server = require('http').Server;
+const http = new Server(app);
 
 app.use(express.static('public'));
+
+const port = 3000;
+http.listen(port, () => debug('listening on port %d', port));
+
+const io = require('socket.io')(http);
+io.on('connection', function(socket) {
+  debug('a user connected');
+  socket.emit('handshake');
+});
