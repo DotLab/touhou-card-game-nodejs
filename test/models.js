45544f859c3d5bae5e1f7e4bff0ad1289fda36b7
Kailang <kailangfu@gmail.com>
2019-02-03 14:00:03 -0600

    Add tests

---
+++ test/models.js
@@ -0,0 +1,19 @@
+const assert = require('chai').assert;
+
+describe('models', () => {
+  const mongoose = require('mongoose');
+  mongoose.connect('mongodb://localhost:27017/tcgTest', {useNewUrlParser: true});
+
+  const models = require('../src/models');
+
+  describe('User', () => {
+    it('can create', async () => {
+      await models.User.create({name: 'abc', salt: 'qwe', hash: 'ret'});
+      const doc = await models.User.findOne({name: 'abc'});
+      assert.isNotNull(doc);
+      assert.strictEqual(doc.name, 'abc');
+      assert.strictEqual(doc.salt, 'qwe');
+      assert.strictEqual(doc.hash, 'ret');
+    });
+  });
+});
