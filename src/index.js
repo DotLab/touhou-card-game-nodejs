f22126e76b1d764c0074dab8a86e1cbe38397be2
Kailang <kailangfu@gmail.com>
2019-02-03 12:03:03 -0600

    Add static file serving

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-const debug = require('debug')('index');
+const debug = require('debug')('tcg');
+const express = require('express');
 const app = require('express')();
 
 const port = 3000;
 app.listen(port, () => debug('listening on port %d', port));
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.use(express.static('public'));
