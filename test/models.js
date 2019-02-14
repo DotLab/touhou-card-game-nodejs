9d1a0fa1e3b63fbfb014e34208650a18f8cc368b
Kailang <kailangfu@gmail.com>
2019-02-13 20:05:14 -0600

    Fix mocha

--- test/models.js
+++ test/models.js
@@ -1,18 +1,29 @@
 const assert = require('chai').assert;
+const mongoose = require('mongoose');
+const {User} = require('../src/models');
 
 describe('models', () => {
-  const mongoose = require('mongoose');
-  mongoose.connect('mongodb://localhost:27017/tcgTest', {useNewUrlParser: true});
+  before(async () => {
+    await mongoose.connect('mongodb://localhost:27017/tcgTest', {useNewUrlParser: true});
+  });
 
-  const models = require('../src/models');
+  after(async () => {
+    await mongoose.disconnect();
+  });
+
   describe('User', () => {
-    it('can create', async () => {
-      await models.User.create({name: 'abc', salt: 'qwe', hash: 'ret'});
-      const doc = await models.User.findOne({name: 'abc'});
+    before(async () => {
+      await User.deleteMany({});
+    });
+
+    it('create', async () => {
+      await User.create({name: 'abc', salt: 'qwe', hash: 'ret', bio: 'ffe'});
+      const doc = await User.findOne({name: 'abc'});
       assert.isNotNull(doc);
       assert.strictEqual(doc.name, 'abc');
       assert.strictEqual(doc.salt, 'qwe');
       assert.strictEqual(doc.hash, 'ret');
+      assert.strictEqual(doc.bio, 'ffe');
     });
   });
 });
