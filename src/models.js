d579b272b11518674aad07a3f94dc4ca79cc5a3c
Ellen Ni <ni_ellen@yahoo.com>
2019-02-17 13:33:02 -0600

    Record model

--- src/models.js
+++ src/models.js
@@ -1,3 +1,22 @@
 const mongoose = require('mongoose');
+const ObjectId = mongoose.SchemaTypes.ObjectId;
 
-exports.User = mongoose.model('User', {name: String, salt: String, hash: String, bio: String});
+exports.User = mongoose.model('User', {
+  name: String,
+  salt: String,
+  hash: String,
+  bio: String,
+  joinDate: Date,
+  lastDate: Date,
+  onlineTime: Number,
+  // cached
+  gameCount: Number,
+  winCount: Number,
+});
+
+// A User has a record of each game they have played
+exports.Record = mongoose.model('Record', {
+  userId: ObjectId,
+  gameId: String,
+  hasWon: Boolean,
+});
