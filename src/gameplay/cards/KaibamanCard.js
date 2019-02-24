67afe510e0a0765c6550f6dc7595a644931fbaa9
Kuo Bao <kuobao2@illinois.edu>
2019-02-24 14:25:41 -0600

    Finish implementation

--- src/gameplay/cards/KaibamanCard.js
+++ src/gameplay/cards/KaibamanCard.js
@@ -1,4 +1,6 @@
 const MonsterCard = require('./MonsterCard');
+const BlueEyesWhiteDragonCard = require('./BlueEyesWhiteDragonCard');
+const Card = require('./Card');
 /**
  * Kaibaman Card
  * @extends MonsterCard
@@ -10,13 +12,24 @@ class KaibamanCard extends MonsterCard {
 
   canInvoke(player) {
     for (let i = 0; i < player.hand.length; i += 1) {
-      if (player.hand[i].name === 'Blue-Eyes White Dragon') return true;
+      if (player.hand[i].name === BlueEyesWhiteDragonCard.Name) return true;
       return false;
     }
   }
 
   invoke(game, player) {
-    game.summo;
+    for (let i = 0; i < player.hand.length; i += 1) {
+      if (player.hand[i].name === BlueEyesWhiteDragonCard.Name) {
+        for (let j = 0; j < player.field.monsterSlots.length; j += 1) {
+          if (player.field.monsterSlots[j] === this) {
+            player.field.monsterSlots[j] = null;
+            player.field.graveyard.push(this);
+            game.summon(i, j, Card.REVEALED, Card.ATTACK);
+            return;
+          }
+        }
+      }
+    }
   }
 }
 
