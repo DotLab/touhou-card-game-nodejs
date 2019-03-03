427f80187f6487748892492da04ca8f21e84f721
Kuo Bao <alexkuobao@gmail.com>
2019-03-03 17:33:08 -0600

    modify some comments

--- test/gameplay/cards/ThousandKnivesCard.js
+++ test/gameplay/cards/ThousandKnivesCard.js
@@ -24,15 +24,13 @@ describe('ThousandKnives', () => {
     assertGameSuccess(game.endTurn());
     // end of player[1]'s turn, not it is player[0]'s turn
     assert.equal(game.turn, 0);
-    // Dark Magic Attack can invoke!
+    // Thousand Knives can invoke!
     assert.isTrue(game.players[0].hand[0].canInvoke(game, game.players[0], ['def', 0]));
     // Invoke!
     assertGameSuccess(game.place(0, 0, Card.REVEALED));
     // player[0] invokes, player[1] takes the effect
     assertGameSuccess(game.invokeSpell(0, ['def', 0]));
-    // All trap and spell card of players[1] destroyed
     assert.equal(game.players[1].field.monsterSlots[0], null);
-    // DMA goes into graveyard
     assert.equal(game.players[1].field.graveyard.length, 1);
     assert.equal(game.players[1].field.graveyard.length, 1);
   });
