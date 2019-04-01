4daad803e6d6339ecfea2cfb61c3ddd412731719
charlie <esculin.hao@gmail.com>
2019-04-01 12:42:57 -0500

    Zaborg the Thunder Monarch refactoring

--- test/gameplay/cards/ZaborgTheThunderMonarchCard.js
+++ test/gameplay/cards/ZaborgTheThunderMonarchCard.js
@@ -58,6 +58,40 @@ describe('ZaborgTheThunderMonarchCard', ()=>{
     assert.equal(game.players[1].field.graveyard.length, 1);
   });
 
+  it('#choose not to activate', ()=> {
+    const game = new Game([{id: 'abc', deck: buildDeck()}, {id: 'def', deck: buildDeck()}]);
+    // player 0 place one monster card
+    assertGameSuccess(game.summon(1, 0, Card.REVEALED, Card.ATTACK));
+    assert.equal(game.players[0].field.monsterSlots[0].name, BlueEyesWhiteDragonCard.Name);
+    assert.equal(game.players[0].field.monsterSlots[0].pose, Card.ATTACK);
+    assert.equal(game.players[0].field.monsterSlots[0].lv, 8);
+    assert.equal(game.players[0].field.monsterSlots[0].atk, BlueEyesWhiteDragonCard.Atk);
+    assert.equal(game.players[0].field.monsterSlots[0].dfs, BlueEyesWhiteDragonCard.Dfs);
+    //  end player 0 turn
+    assertGameSuccess(game.endTurn());
+    // now it is player 1 turn
+    assert.equal(game.turn, 1);
+    // player 1 summons Zaborg the frost monarch
+    assertGameSuccess(game.summon(0, 0, Card.REVEALED, Card.ATTACK));
+    assert.equal(game.players[1].field.monsterSlots[0].name, ZaborgTheThunderMonarchCard.Name);
+    assert.equal(game.players[1].field.monsterSlots[0].pose, Card.ATTACK);
+    // activates Zaborg power on two two spell cards of player 0
+    assertGameSuccess(game.invokeMonsterEffect(0, [null, null]));
+    // now player 0 monster card should be gone to graveyard
+    assert.equal(game.players[0].field.graveyard.length, 0);
+    // player 1 then place a monster card for player 0 to destroy
+    assertGameSuccess(game.summon(0, 1, Card.REVEALED, Card.ATTACK));
+    assert.equal(game.players[1].field.monsterSlots[1].name, BlueEyesWhiteDragonCard.Name);
+    assert.equal(game.players[1].field.monsterSlots[1].pose, Card.ATTACK);
+    assert.equal(game.players[1].field.monsterSlots[1].lv, 8);
+    assert.equal(game.players[1].field.monsterSlots[1].atk, BlueEyesWhiteDragonCard.Atk);
+    assert.equal(game.players[1].field.monsterSlots[1].dfs, BlueEyesWhiteDragonCard.Dfs);
+    //  end player 1 turn
+    assertGameSuccess(game.endTurn());
+    // now it is player 0 turn
+    assert.equal(game.turn, 0);
+  });
+
   it('#cannot activate', ()=>{
     const game = new Game([{id: 'abc', deck: buildDeck()}, {id: 'def', deck: buildDeck()}]);
     // player 0 place a monster cards
