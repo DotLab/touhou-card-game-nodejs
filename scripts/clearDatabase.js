3b1314cce0b0fa445f3f693aa874bfdc329ab526
Kailang <kailangfu@gmail.com>
2019-04-04 14:10:01 -0500

    Fix coverage

--- scripts/clearDatabase.js
+++ scripts/clearDatabase.js
@@ -2,7 +2,7 @@
   const mongoose = require('mongoose');
   mongoose.connect('mongodb://localhost:27017/tcg', {useNewUrlParser: true});
 
-  const {User} = require('./models');
+  const {User} = require('../src/models');
 
   await User.deleteMany({});
 
