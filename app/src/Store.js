297786a7c83703c88e267b6aee9ca725c2ff0f44
Kailang <kailangfu@gmail.com>
2019-04-03 22:26:46 -0500

    Add styling to store

--- app/src/Store.js
+++ app/src/Store.js
@@ -24,14 +24,14 @@ export default class Store extends React.Component {
 
   async buyLife() {
     if (this.app.state.user.spiritPointsCount >= LIFE_UPGRADE_COST) {
-      const points = await this.app.genericApi0('cl_store_buy_life');
+      const res = await this.app.genericApi0('cl_store_buy_life');
       this.app.setState({user: {
         ...this.app.state.user,
-        spiritPointsCount: points.spiritPointsCount, 
-        lifeUpgrade: points.lifeUpgrade,
+        spiritPointsCount: res.spiritPointsCount, 
+        lifeUpgrade: res.lifeUpgrade,
       }});
     } else {
-      this.setState({message: 'Not enough Spirit Points'});
+      this.app.setState({message: 'Not enough Spirit Points'});
     }
   }
 
@@ -39,20 +39,23 @@ export default class Store extends React.Component {
     const s = this.state;
     const user = this.app.state.user;
     const isLoggedIn = user !== null;
-    return <div>
-      {isLoggedIn && <button onClick={this.toggleView}>Store</button>}
-      {s.showStore && <div>
-        <table>
+
+    return <div className="container mt-2">
+      {isLoggedIn && <button className="btn btn-info" onClick={this.toggleView}>Store</button>}
+      {s.showStore && <div className="mt-2">
+        <table className="table table-striped my-0 py-0">
+          <thead>
+            <tr>
+              <th>Item Name</th>
+              <th>Item Cost</th>
+              <th>Action</th>
+            </tr>
+          </thead>
           <tbody>
             <tr>
-              <th align='left'>Item </th>
-              <th align='left'>Cost </th>
-              <th> </th>
-            </tr>
-            <tr>
-              <td>Life +{LIFE_UPGRADE_AMOUNT} </td>
-              <td>-{LIFE_UPGRADE_COST} Spirit Points </td>
-              <td><button onClick={this.buyLife}>Buy</button> </td>
+              <td className="Va(m)!">Life +{LIFE_UPGRADE_AMOUNT} L</td>
+              <td className="Va(m)!">{LIFE_UPGRADE_COST} SP</td>
+              <td className="Va(m)!">{user.spiritPointsCount >= LIFE_UPGRADE_COST ? <button className="btn btn-danger" onClick={this.buyLife}>Buy</button> : "Too Poor To Buy"}</td>
             </tr>
             {/* <tr>
               <td>Card Upgrade </td>
