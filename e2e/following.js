82a3fb61b23eb6100e031ff888281e6e1f5351da
BINGXUHAN <bhan15@illinois.edu>
2019-04-21 18:10:49 -0500

    selenium tests doc

--- e2e/following.js
+++ e2e/following.js
@@ -5,30 +5,36 @@ const {createWebdriver, E2E_INDEX} = require('./utils');
 const {By, until} = require('selenium-webdriver');
 
 describe('followingAndFollower', () => {
+  // start a web
   const driver = createWebdriver();
 
   after(() => {
+    // close the web
     driver.quit();
   });
 
   it('open index page', async () => {
+    // go to the game page
     await driver.get(E2E_INDEX);
-    // const createNewTab = await Key.chord(Key.CONTROL, Key.);
+    // open a new tab
     await driver.executeScript(`window.open('${E2E_INDEX}');`);
-    // Key.chord
+    // go to the game page
     await driver.get(E2E_INDEX);
+    // list all the tabs
     const tabHandles =await driver.getAllWindowHandles();
+    // switch to one of the tab
     await driver.switchTo().window(tabHandles[0]);
-    // console.log(driver.getAllWindowHandles().length);
   });
 
   async function register(regName, regPassword) {
+    // find the register name field
     const registerName = await driver.findElement(By.name('registerName'));
+    // find the register password field
     const registerPassword = await driver.findElement(By.name('registerPassword'));
-
+    // clear both field
     await registerName.clear();
     await registerPassword.clear();
-
+    // fill both field
     await registerName.sendKeys(regName);
     await registerPassword.sendKeys(regPassword);
 
@@ -41,19 +47,23 @@ describe('followingAndFollower', () => {
 
 
   it('register1', async () => {
+    // register the user1
     await register('following1', 'followingPassword1');
   });
 
   it('register2', async () => {
+    // register the user2
     await register('following2', 'followingPassword2');
   });
 
   it('login1', async () => {
+    // find the login name and password field
     const loginName = await driver.findElement(By.name('loginName'));
     const loginPassword = await driver.findElement(By.name('loginPassword'));
-
+    // clear both field
     await loginName.clear();
     await loginPassword.clear();
+    // fill both field
     await loginName.sendKeys('following1');
     await loginPassword.sendKeys('followingPassword1');
     await loginName.submit();
@@ -64,24 +74,25 @@ describe('followingAndFollower', () => {
   });
 
   it('following', async () => {
+    // toggle playerlist
     const playerListButton = await driver.findElement(By.name('togglePlayer'));
     await playerListButton.click();
-
+    // follow the other player
     const followButton = await driver.findElement(By.name('following2'));
     await followButton.click();
-
+    // toggle stats
     const toggleStat = await driver.findElement(By.name('toggleStats'));
     await toggleStat.click();
-
+    // check whehther the other player is added to the following list
     const following = await driver.findElement(By.name('followingList'));
     assert.equal(await following.getText(), 'following2');
   });
 
   it('login2', async () => {
-    // const switchTab = await Key.chord(Key.CONTROL, Key.TAB);
+    // switch to another tab
     const tabHandles =await driver.getAllWindowHandles();
     await driver.switchTo().window(tabHandles[1]);
-
+    // log in to the other player
     const loginName = await driver.findElement(By.name('loginName'));
     const loginPassword = await driver.findElement(By.name('loginPassword'));
     loginName.clear();
@@ -96,6 +107,7 @@ describe('followingAndFollower', () => {
   });
 
   it('follower', async () => {
+    // toggle stat and check if user1 is in the follower list
     const toggleStat = await driver.findElement(By.name('toggleStats'));
     await toggleStat.click();
 
--- e2e/registerLogin.js
+++ e2e/registerLogin.js
@@ -4,22 +4,26 @@ const {createWebdriver, E2E_INDEX} = require('./utils');
 const {By, until} = require('selenium-webdriver');
 
 describe('registerLogin', () => {
+  // start a web
   const driver = createWebdriver();
 
   after(() => {
+    // close the web
     driver.quit();
   });
 
   it('open index page', async () => {
+    // go to the game page
     await driver.get(E2E_INDEX);
   });
-
   it('register', async () => {
-    // driver.navigate().refresh();
+    // find the register name field
     const registerName = await driver.findElement(By.name('registerName'));
+    // find the register password field
     const registerPassword = await driver.findElement(By.name('registerPassword'));
-
+    // fill the register name field
     await registerName.sendKeys('testName');
+    // fill the register password field
     await registerPassword.sendKeys('testPassword');
     await registerName.submit();
 
@@ -29,10 +33,13 @@ describe('registerLogin', () => {
   });
 
   it('login', async () => {
+    // find the login name field
     const loginName = await driver.findElement(By.name('loginName'));
+    // find the login password field
     const loginPassword = await driver.findElement(By.name('loginPassword'));
-
+    // fill the login name field
     await loginName.sendKeys('testName');
+    // fill the login password field
     await loginPassword.sendKeys('testPassword');
     await loginName.submit();
 
